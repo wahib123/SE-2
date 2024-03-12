54cd721380a15c0fd89eca9a2b29e224c5705902
Wahibpeerbits <wahib.s@peerbits.com>
2024-03-12 17:31:49 +0530

    login page design

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import Saly from "../assets/Saly-3.svg";
+import Saly2 from "../assets/Saly-2.svg";
 
 const Login = () => {
   return (
-    <div className="grid grid-cols-2 h-[100vh]">
-      <div className=" bg-[#ECBC76]">01</div>
-      <div className=" bg-[#ffffff]">02</div>
+    <div className="h-[100vh] login-parent-container relative flex items-center justify-center">
+      <span className="absolute text-lg text-[#C6553B] top-[5px] left-[10px] font-poppins">
+        Your Logo Here
+      </span>
+      <div className="flex items-center justify-center w-[100%]">
+        <div className="w-[33.33%] grid">
+          <img
+            src={Saly}
+            alt="soly"
+            className="justify-self-end soly-img soly-small border-b border-black"
+          />
+        </div>
+        <div className="w-[33.33%] login-container rounded px-3 py-4 border border-solid border-black">
+          <span className="font-poppins">Container</span>
+        </div>
+        <img src={Saly2} alt="soly" className="w-[33.33%] soly-img" />
+      </div>
     </div>
   );
 };
