5ba8bc2b18ee6a6149f47fbe48955d9f75bf78d0
Wahibpeerbits <wahib.s@peerbits.com>
2024-03-13 01:58:43 +0530

    signup page design

--- src/routes/routeConstant.ts
+++ src/routes/routeConstant.ts
@@ -2,11 +2,16 @@ import { lazy } from "react";
 
 const Dashboard = lazy(async () => await import("../pages/Dashboard"));
 const Login = lazy(async () => await import("../pages/Login"));
+const SignUp = lazy(async () => await import("../pages/SignUp"));
 
 const publicRoutes = [
   {
     path: "/login",
     element: Login,
+  },
+  {
+    path: "/signup",
+    element: SignUp,
   },
 ];
 
